eeba13e098d32afa22ae5ae523568d8aa3d9341d
“Josh” <jbrockwilliams@gmail.com>
2023-02-21 15:53:44 -0600

    Alot OF CHAGES

--- Game.py
+++ Game.py
@@ -11,6 +11,17 @@ from pygame import mixer
 #Instantiate mixer
 mixer.init()
 
+"""
+ 
+           ______            _     _                       ______  __                        
+    _  _  |_   _ \          / |_  / |_                   .' ___  |[  |                       
+  _| || |_  | |_) | __   _ `| |-'`| |-' .--.   _ .--.   / .'   \_| | |  ,--.   .--.   .--.   
+ |_| || |_| |  __'.[  | | | | |   | | / .'`\ \[ `.-. |  | |        | | `'_\ : ( (`\] ( (`\]  
+ |_| || |_|_| |__) || \_/ |,| |,  | |,| \__. | | | | |  \ `.___.'\ | | // | |, `'.'.  `'.'.  
+   |_||_| |_______/ '.__.'_/\__/  \__/ '.__.' [___||__]  `.____ .'[___]\'-;__/[\__) )[\__) ) 
+                                                                                             
+ 
+"""
 buttons = []
 class Button:
 	def __init__(self,text,width,height,pos,elevation):
@@ -76,7 +87,7 @@ fps = 60
 WIDTH = 640
 HEIGHT = 480
 display = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption("2048 Game by Izzy and Josh")
+pygame.display.set_caption("2048+")
 gui_font = pygame.font.Font('freesansbold.ttf',24)
 print(pygame.font.get_fonts())
 font = pygame.font.Font('freesansbold.ttf',24)
@@ -84,31 +95,73 @@ font = pygame.font.Font('freesansbold.ttf',24)
 button3 = Button('USA',75,40,(528,25),5)
 button2 = Button('Brazil',75,40,(528,75),5)
 button1 = Button('Finland',75,40,(528,125),5)
+button4 = Button('Grunge',75,40,(528,175),5)
+kitbutton = Button('Kit2',90,40,(0,175),5)
+kit1button = Button('Kit1',90,40,(0,125),5)
+kit3button = Button('Kit3',90,40,(0,75),5)
+kit4button = Button('Kit4',90,40,(0,25),5)
 
 s = 'sounds'
 
+"""
+ 
+           _____                          __    _              ______                                  __         
+    _  _  |_   _|                        |  ]  (_)           .' ____ \                                |  ]        
+  _| || |_  | |       .--.   ,--.    .--.| |   __   _ .--.   | (___ \_|  .--.   __   _   _ .--.   .--.| |  .--.   
+ |_| || |_| | |   _ / .'`\ \`'_\ : / /'`\' |  [  | [ `.-. |   _.____`. / .'`\ \[  | | | [ `.-. |/ /'`\' | ( (`\]  
+ |_| || |_|_| |__/ || \__. |// | |,| \__/  |   | |  | | | |  | \____) || \__. | | \_/ |, | | | || \__/  |  `'.'.  
+   |_||_| |________| '.__.' \'-;__/ '.__.;__] [___][___||__]  \______.' '.__.'  '.__.'_/[___||__]'.__.;__][\__) ) 
+                                                                                                                  
+ 
+"""
 
-# bass = pygame.mixer.Sound(os.path.join(s, 'kick-bass.mp3'))
-# snare = pygame.mixer.Sound(os.path.join(s, 'snare.mp3'))
-# tom1 = pygame.mixer.Sound(os.path.join(s, 'tom-1.mp3'))
-# tom2 = pygame.mixer.Sound(os.path.join(s, 'tom-2.mp3'))
 
+Kit = {
+'hi_hat' : mixer.Sound('kit1/hi hat.WAV'),
+'clap' : mixer.Sound('kit1/clap.WAV'),
+'crash' : mixer.Sound('kit1/crash.WAV'),
+'kick' : mixer.Sound('kit1/kick.WAV'),
+'snare' : mixer.Sound('kit1/snare.WAV'),
+'tom' : mixer.Sound('kit1/tom.WAV')
+}
+tempKit = Kit
+Kit2 = {
+'hi_hat' : mixer.Sound('kit2/hi hat.WAV'),
+'clap' : mixer.Sound('kit2/clap.WAV'),
+'crash' : mixer.Sound('kit2/crash.WAV'),
+'kick' : mixer.Sound('kit2/kick.WAV'),
+'snare' : mixer.Sound('kit2/snare.WAV'),
+'tom' : mixer.Sound('kit2/tom.WAV')
+}
+Kit3 = {
+'hi_hat' : mixer.Sound('kit3/hi hat.WAV'),
+'clap' : mixer.Sound('kit3/clap.WAV'),
+'crash' : mixer.Sound('kit3/crash.WAV'),
+'kick' : mixer.Sound('kit3/kick.WAV'),
+'snare' : mixer.Sound('kit3/snare.WAV'),
+'tom' : mixer.Sound('kit3/tom.WAV')
+}
+Kit4 = {
+'hi_hat' : mixer.Sound('kit4/hi hat.WAV'),
+'clap' : mixer.Sound('kit4/clap.WAV'),
+'crash' : mixer.Sound('kit4/crash.WAV'),
+'kick' : mixer.Sound('kit4/kick.WAV'),
+'snare' : mixer.Sound('kit4/snare.WAV'),
+'tom' : mixer.Sound('kit4/tom.WAV')
+}
 
+"""
+ 
+              ______         __                   _________                            __          _                 
+    _  _    .' ___  |       [  |                 |  _   _  |                          [  |        / |_               
+  _| || |_ / .'   \_|  .--.  | |  .--.   _ .--.  |_/ | | \_|.---.  _ .--..--.  _ .--.  | |  ,--. `| |-'.---.  .--.   
+ |_| || |_|| |       / .'`\ \| |/ .'`\ \[ `/'`\]     | |   / /__\\[ `.-. .-. |[ '/'`\ \| | `'_\ : | | / /__\\( (`\]  
+ |_| || |_|\ `.___.'\| \__. || || \__. | | |        _| |_  | \__., | | | | | | | \__/ || | // | |,| |,| \__., `'.'.  
+   |_||_|   `.____ .' '.__.'[___]'.__.' [___]      |_____|  '.__.'[___||__||__]| ;.__/[___]\'-;__/\__/ '.__.'[\__) ) 
+                                                                              [__|                                   
+ 
+"""
 
-# ValueColor = {
-#   'purple': (128, 0, 128),
-#   'silver': (192, 192, 192),
-#   'gray': (128, 128, 128),
-#   'Red': (255, 0, 0),
-#   'Maroon': (128, 0, 0),
-#   'yellow': (255, 255, 0),
-#   'olive': (128, 128, 74),
-#   'lime': (0, 255, 0),
-#   'Green': (0, 128, 0),
-#   'Aqua': (0, 255, 255),
-#   'teal': (0, 128, 128),
-#   'blue': (0, 0, 255),
-# }
 GameColor = {
     0: (204, 192, 179),
           2: (238, 228, 218),
@@ -146,6 +199,25 @@ GameColor_USA = {
   'other': (0,0,0),
   'bg': (179, 25, 66),
 }
+GameColor_Grunge = {
+      0: (204, 192, 179),
+          2: (238, 228, 218),
+          4: (237, 224, 200),
+          8: (160, 160, 160),
+          16: (128, 128, 128),
+          32: (97, 97, 97),
+          64: (102, 0, 51),
+          128: (153, 0, 76),
+          256: (255, 0, 0),
+          512: (255, 0, 0),
+          1024: (255, 0, 0),
+          2048: (255, 0, 0),
+  'light text': (255, 255, 255),
+  'dark text': (255, 0, 0),
+  'other': (0,0,0),
+  'bg': (0, 0, 0),
+}
+
 GameColor_Brazil = {
       0: (204, 192, 179),
           2: (238, 228, 218),
@@ -185,7 +257,7 @@ GameColor_Finland = {
 }
 
 #List of values for board
-tile_vals = [[0 for _ in range(4)] for _ in range(4)]
+tile_vals = [[2048 for _ in range(4)] for _ in range(4)]
 
 game_over = False
 
@@ -198,15 +270,24 @@ buttons = []
 
 score = 0
 
+"""
+ 
+              ______                              ________                          _    _                          
+    _  _    .' ___  |                            |_   __  |                        / |_ (_)                         
+  _| || |_ / .'   \_|  ,--.   _ .--..--.  .---.    | |_ \_|__   _   _ .--.   .---.`| |-'__   .--.   _ .--.   .--.   
+ |_| || |_|| |   ____ `'_\ : [ `.-. .-. |/ /__\\   |  _|  [  | | | [ `.-. | / /'`\]| | [  |/ .'`\ \[ `.-. | ( (`\]  
+ |_| || |_|\ `.___]  |// | |, | | | | | || \__.,  _| |_    | \_/ |, | | | | | \__. | |, | || \__. | | | | |  `'.'.  
+   |_||_|   `._____.' \'-;__/[___||__||__]'.__.' |_____|   '.__.'_/[___||__]'.___.'\__/[___]'.__.' [___||__][\__) ) 
+                                                                                                                    
+ 
+"""
+
 def draw_over():
     pygame.draw.rect(display, 'black', [50, 50, 300, 100], 0, 10)
     game_over_text1 = font.render('Game Over!', True, 'white')
     game_over_text2 = font.render('Press Enter to Restart', True, 'white')
     display.blit(game_over_text1, (130, 65))
     display.blit(game_over_text2, (70, 105))
-
-def getcolor(i):
-  return GameColor[i]
 
 
 #function that takes turn for user 
@@ -331,18 +412,34 @@ def drawTiles(tile_val):
 
 while True:
   timer.tick(fps)
-  display.fill('tan')
+  display.fill(GameColor['bg'])
   drawBoard()
   drawTiles(tile_vals)
+  button4.draw()
   button3.draw()
   button2.draw()
   button1.draw()
+  kitbutton.draw()
+  kit1button.draw()
+  kit3button.draw()
+  kit4button.draw()
+  
+  if kitbutton.pressed:
+    Kit = Kit2
+  if kit1button.pressed:
+    Kit = tempKit
+  if kit3button.pressed:
+    Kit = Kit3
+  if kit4button.pressed:
+    Kit = Kit4
   if button1.pressed:
       GameColor = GameColor_Finland
   elif button2.pressed:
       GameColor = GameColor_Brazil
   elif button3.pressed:
       GameColor = GameColor_USA
+  elif button4.pressed:
+    GameColor = GameColor_Grunge
   else:
       pass
 
@@ -364,42 +461,42 @@ while True:
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_UP:
         direction = 'UP'
-        mixer.music.load('crash.mp3')
         #Set preferred volume
         mixer.music.set_volume(0.2)
         #Play the music
-        mixer.music.play()
+        Kit['crash'].play()
       elif event.key == pygame.K_DOWN:
         direction = 'DOWN'
-        mixer.music.load('snare.mp3')
         #Set preferred volume
         mixer.music.set_volume(0.2)
         #Play the music
-        mixer.music.play()
+        Kit['clap'].play()
       elif event.key == pygame.K_LEFT:
         direction = 'LEFT'
-        mixer.music.load('tom-1.mp3')
         #Set preferred volume
         mixer.music.set_volume(0.2)
         #Play the music
-        mixer.music.play()
+        Kit['snare'].play()
       elif event.key == pygame.K_RIGHT:
         direction = 'RIGHT'
-        mixer.music.load('tom-2.mp3')
         #Set preferred volume
         mixer.music.set_volume(0.2)
         #Play the music
-        mixer.music.play()
+        Kit['tom'].play()
       elif event.key == pygame.K_SPACE:
-        mixer.music.load('kick-bass.mp3')
         #Set preferred volume
         mixer.music.set_volume(0.2)
         #Play the music
-        mixer.music.play()
+        Kit['kick'].play()
+      elif event.key == pygame.K_RSHIFT:
+        #Set preferred volume
+        mixer.music.set_volume(0.2)
+        #Play the music
+        Kit['hi_hat'].play()
       
       if game_over:
                 if event.key == pygame.K_RETURN:
-                    board_values = [[0 for _ in range(4)] for _ in range(4)]
+                    tile_vals = [[0 for _ in range(4)] for _ in range(4)]
                     spawn_new = True
                     init_count = 0
                     score = 0
